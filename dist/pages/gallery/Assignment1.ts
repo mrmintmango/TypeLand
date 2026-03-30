a81e45abd180b96627d09b6e119b4627136563f5
MrMintCode <ruben.matos.imp@gmail.com>
2026-03-30 11:42:09 -0600

    Submission Gallery is now Open! Also the learning log has just dropped.

---
+++ dist/pages/gallery/Assignment1.ts
@@ -0,0 +1,18 @@
+// Assignment 1: TypeScript Basics
+// Instructions: Complete the following exercises to practice TypeScript fundamentals
+
+// TODO: Declare a variable called 'studentName' of type string
+
+// TODO: Declare a variable called 'studentAge' of type number
+
+// TODO: Declare a variable called 'isEnrolled' of type boolean
+
+// TODO: Create a function called 'greet' that takes a name parameter (string)
+// and returns a greeting message (string)
+
+// TODO: Create an array of numbers called 'grades' with at least 3 values
+
+// TODO: Create an object called 'course' with properties: title (string),
+// credits (number), and instructor (string)
+
+// Write your code below:
